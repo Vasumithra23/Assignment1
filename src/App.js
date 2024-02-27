e9c12778f45d9b34e9817ae75e8858ab5c9448d5
vasumithra.gorrepati <vasumithra.gorrepati@pal.tech>
2024-02-27 12:34:15 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import logo from './delete.svg';
 
-function App() {
+
+const Users=[
+  {id : 1,fname : 'vasumithra', lname : 'gorrepati', email : 'xyz@gmail.com'},
+  {id : 2,fname : 'Mahesh', lname : 'gorrepati', email : 'xyz1@gmail.com'},  
+];
+
+
+export default function App() {
+  const [user,setuser] = useState(Users);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='dashboard'>
+      <UserForm user={user} setuser={setuser}/>
+      <DisplayUsers user={user} setuser={setuser} />
     </div>
   );
 }
 
-export default App;
+function UserForm({user,setuser}){
+  const [userForm,setuserForm] = useState({
+    id : 0,
+    fname : "",
+    lname : "",
+    email : ""
+  });
+  const[isValid,setIsValid] = useState(true);
+
+  const handleChange = (event) => {
+    const id = event.target.id;
+    const value = event.target.value;
+    switch (id) {
+      case "id" : setuserForm((prev)=>{return {...prev,id : Number(value)}}); break;
+      case "fname" : setuserForm((prev)=>{return {...prev,fname : value}}); break;
+      case "lname" : setuserForm((prev)=>{return {...prev,lname : value}}); break;
+      case "email" : setuserForm((prev)=>{return {...prev,email : value}}); break;
+    }
+  };
+  const onFormSubmit =(e) => {
+    
+   const isDuplicate = user.filter((user) => user.id === Number(userForm.id)).length>0;
+   if(isDuplicate)
+   {
+     setIsValid(!isValid);
+     e.preventDefault();
+   }
+   else{
+   setuser([...user,{...userForm}]);
+   setIsValid(!isValid);
+   e.preventDefault();
+   e.target.reset();
+   }
+  }
+  return(
+   <div className='form-div'>
+   <form onSubmit={onFormSubmit} className='form'>
+   <label htmlFor="id">Id </label>
+   <input id="id" type="number" onChange={handleChange}></input><br></br>
+   <label htmlFor="fname">FirstName </label>
+   <input id="fname" onChange={handleChange}></input><br></br>
+   <label htmlFor="lname">LastName </label>
+   <input id="lname" onChange={handleChange}></input><br></br>
+   <label htmlFor="email">Email </label>
+   <input id="email" onChange={handleChange}></input><br></br>
+   <button type='submit'>
+    Add User
+   </button>
+    {!isValid ? (<p style={{color: "red"}}>Id already exists</p>):(null)}
+   </form>
+   </div>
+  );
+}
+
+function DisplayUsers({user,setuser}){
+
+const onhandledelete = (id)=>
+{
+  setuser(user.filter((t) => t.id !== id))
+}
+if(user.length>0){
+return (
+  <div className='display'>
+  <TableContainer component={Paper}>
+  <div className='tableheading'>List of available users</div>
+  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableHead>
+      <TableRow>
+      <TableCell align="center"><b>Id</b></TableCell>
+        <TableCell align="center"><b>FirstName</b></TableCell>
+        <TableCell align="center"><b>LastName</b></TableCell>
+        <TableCell align="center"><b>Email</b></TableCell>
+        <TableCell align="center"><b>Action</b></TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {user.map((row) => (
+        <TableRow
+        key={row.id}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        >
+          <TableCell align="center">{row.id}</TableCell>
+          <TableCell align="center">{row.fname}</TableCell>
+          <TableCell align="center">{row.lname}</TableCell>
+          <TableCell align="center">{row.email}</TableCell>
+          <TableCell align="center"><img className='delete-icon' src={logo} onClick={()=> onhandledelete(row.id)}></img></TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+</TableContainer>
+</div>
+)
+}
+else{
+  return <div>No Users to display</div>
+}
+}
+
+// function AddUser(props){
+//   console.log('df');
+//   const [adduser,setadduser] = useState(Users);
+//   setadduser(...adduser,{
+//     fname : props.fname,
+//     lname : props.lname,
+//     email : props.email,
+//   });
+// }
